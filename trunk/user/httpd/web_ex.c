c50bd6482a1f02868b7394b468ce97ccd080523b
PiGGy52511 <32626479+PiGGy52511@users.noreply.github.com>
2026-03-05 14:29:46 +0800

    Update web_ex.c

--- trunk/user/httpd/web_ex.c
+++ trunk/user/httpd/web_ex.c
@@ -2584,12 +2584,12 @@ ej_firmware_caps_hook(int eid, webs_t wp, int argc, char **argv)
 #if defined(APP_VPNSVR)
 	int found_app_vpnsvr = 1;
 #else
-	int found_app_vpnsvr = 1;
+	int found_app_vpnsvr = 0;
 #endif
 #if defined(APP_VPNCLI)
 	int found_app_vpncli = 1;
 #else
-	int found_app_vpncli = 1;
+	int found_app_vpncli = 0;
 #endif
 #if defined(USE_IPV6)
 	int has_ipv6 = 1;
